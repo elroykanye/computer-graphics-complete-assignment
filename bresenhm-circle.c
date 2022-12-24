97bdeabed2228d1f916b1782e0f12f61748d58b8
kimbi619 <kimbidarl@gmail.com>
2022-12-24 14:14:09 +0100

    yours sincerely

--- bresenhm-circle.c
+++ bresenhm-circle.c
@@ -3,6 +3,7 @@
 int main(int argc, char *argv[])
 {
     printf("I am a yam\n");
+    printf("you are a bigger yam, be proud of yourself :D ");
 
     return 0;
 }
--- bresenhm-line.c
+++ bresenhm-line.c
@@ -3,6 +3,7 @@
 int main(int argc, char *argv[])
 {
     printf("I am an elaborate yam");
+    printf("there is no bigger yam than you, THANK YOU");
 
     return 0;
 }
